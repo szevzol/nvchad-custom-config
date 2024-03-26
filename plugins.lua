060c8a09e02fa9d807acac4fbbc18010b49a63cf
Zoltan Szeverenyi <szevzol@gmail.com>
2024-03-26 23:13:06 +0100

    Use opts when adding extension to telescope

--- plugins.lua
+++ plugins.lua
@@ -108,16 +108,15 @@ local plugins = {
         version = "^1.0.0",
       },
     },
-    init = function()
+    opts = function()
       local telescope = require("telescope")
       local lga_actions = require("telescope-live-grep-args.actions")
 
       telescope.setup {
         extensions = {
           live_grep_args = {
-            auto_quoting = false, -- enable/disable auto-quoting
-            -- define mappings, e.g.
-            mappings = {          -- extend mappings
+            auto_quoting = true,
+            mappings = {
               i = {
                 ["<C-k>"] = lga_actions.quote_prompt(),
                 ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
