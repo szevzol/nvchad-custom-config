f95bced29401b3b3ecdc8a778f8bc295f0aa99b1
Zoltan Szeverenyi <szevzol@gmail.com>
2024-03-26 23:13:05 +0100

    Add transparency to the theme
    In Windows Terminal it looks good.

--- chadrc.lua
+++ chadrc.lua
@@ -1,7 +1,10 @@
 ---@type ChadrcConfig
 local M = {}
 
-M.ui = { theme = 'catppuccin' }
+M.ui = {
+  theme = 'catppuccin',
+  transparency = true,
+}
 M.plugins = "custom.plugins"
 M.mappings = require "custom.mappings"
 
