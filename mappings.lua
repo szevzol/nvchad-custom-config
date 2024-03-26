851b55c518f18cd51e3096fa922d496534096e16
Zoltan Szeverenyi <szevzol@gmail.com>
2024-03-26 23:13:05 +0100

    Add live grep args extension to telescope

--- mappings.lua
+++ mappings.lua
@@ -4,7 +4,8 @@ M.abc = {
   n = {
     ["<leader>op"] = { "<cmd> Telescope neovim-project history<CR>", "Open recent project" },
     ["<leader>fp"] = { "<cmd> Telescope neovim-project discover<CR>", "Discover projects" },
-    ["<leader>X"] = { "<cmd> lua require(\"nvchad.tabufline\").closeAllBufs()<CR>", "Close all buffers" },
+    ["<leader>fg"] = { "<cmd> lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>", "Live grep with glob" },
+    ["<leader>X"]  = { "<cmd> lua require(\"nvchad.tabufline\").closeAllBufs()<CR>", "Close all buffers" },
   },
 }
 
--- plugins.lua
+++ plugins.lua
@@ -98,5 +98,37 @@ local plugins = {
       }
     end
   },
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      {
+        "nvim-telescope/telescope-live-grep-args.nvim",
+        -- This will not install any breaking changes.
+        -- For major updates, this must be adjusted manually.
+        version = "^1.0.0",
+      },
+    },
+    init = function()
+      local telescope = require("telescope")
+      local lga_actions = require("telescope-live-grep-args.actions")
+
+      telescope.setup {
+        extensions = {
+          live_grep_args = {
+            auto_quoting = false, -- enable/disable auto-quoting
+            -- define mappings, e.g.
+            mappings = {          -- extend mappings
+              i = {
+                ["<C-k>"] = lga_actions.quote_prompt(),
+                ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+                ["<C-t>"] = lga_actions.quote_prompt({ postfix = " -t" }),
+              },
+            },
+          }
+        }
+      }
+      require("telescope").load_extension("live_grep_args")
+    end,
+  }
 }
 return plugins
