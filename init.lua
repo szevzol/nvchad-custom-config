7a7870cdc0b0301f9024fbc8b4a66c461d42e2eb
Zoltan Szeverenyi <szevzol@gmail.com>
2024-12-09 23:29:39 +0100

    Add toggle diagnostics

--- init.lua
+++ init.lua
@@ -46,8 +46,14 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
 
 vim.fn.sign_define('DapBreakpoint', { text = '🟥', texthl = '', linehl = '', numhl = '' })
 vim.fn.sign_define('DapStopped', { text = '▶️', texthl = '', linehl = '', numhl = '' })
---vim.keymap.set('n', '<F5>', require 'nvim-dap'.continue)
+-- vim.keymap.set('n', '<F5>', require 'nvim-dap'.continue)
 -- vim.keymap.set('n', '<F10>', require 'dap'.step_over)
 -- vim.keymap.set('n', '<F11>', require 'dap'.step_into)
 -- vim.keymap.set('n', '<F12>', require 'dap'.step_out)
 -- vim.keymap.set('n', '<leader>b', require 'dap'.toggle_breakpoint)
+
+local toggle_diagnostics = function()
+  vim.diagnostic.enable(not vim.diagnostic.is_enabled())
+end
+
+vim.keymap.set("n", "<leader>td", toggle_diagnostics, { desc = "Toggle Diagnostics" })
